df8e5bc76a50234ebeee705e77d5774a822849e4
Antonio Poloni <antoniopolonijr@gmail.com>
2026-02-06 17:22:25 -0300

    feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+import assert from "node:assert";
+import { dirname } from "node:path";
 import type { NextConfig } from "next";
 
+assert(process.env.BLOB_BASE_URL, "you must have defined BLOB_BASE_URL");
+
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL(`${process.env.BLOB_BASE_URL}/**`)],
+  },
+  turbopack: {
+    root: dirname(__filename),
+  },
 };
 
 export default nextConfig;
